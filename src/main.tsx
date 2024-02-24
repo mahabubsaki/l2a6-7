53ac1494004863a40d76f89fa5c9361a5b584a11
mahabubsaki <md929820@gmail.com>
2024-02-24 22:56:40 +0600

    installation and setup done

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { ChakraProvider } from '@chakra-ui/react';
+import './index.css';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux/store/index.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
-)
+);
---
+++ src/redux/store/index.ts
@@ -0,0 +1,34 @@
+import { configureStore } from '@reduxjs/toolkit';
+import demoSlice from '../features/demo/demoSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import { baseApi } from '../api/baseAPI';
+
+
+const persistConfig = {
+    key: 'auth',
+    storage,
+};
+
+const persistedDemoSlice = persistReducer(persistConfig, demoSlice);
+
+export const store = configureStore({
+    reducer: {
+        demo: persistedDemoSlice,
+        [baseApi.reducerPath]: baseApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            }
+        }).concat(baseApi.middleware),
+
+});
+
+export const persistor = persistStore(store);
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
