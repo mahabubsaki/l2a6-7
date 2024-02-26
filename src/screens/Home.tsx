1aa7e056afca239b93ca0928073eade25e617ccb
mahabubsaki <md929820@gmail.com>
2024-02-27 05:50:22 +0600

    testimonial done

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,6 +3,7 @@ import Banner from '../components/ui/Banner';
 import LowerBanner from '../components/ui/LowerBanner';
 import ContainerFluid from '../components/reusable/ContainerFluid';
 import TopRelief from '../components/ui/TopRelief';
+import Testimonial from '../components/ui/Testimonial';
 
 const Home = () => {
     return (
@@ -11,6 +12,10 @@ const Home = () => {
             <LowerBanner />
             <div className='px-4 lg:px-0'>
                 <TopRelief />
+            </div>
+            <Testimonial />
+            <div className='h-[12000px]'>
+
             </div>
         </ContainerFluid>
     );
