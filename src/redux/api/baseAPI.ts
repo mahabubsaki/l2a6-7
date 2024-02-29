36e03f9334a13da376e33de49b494ad98386682f
mahabubsaki <md929820@gmail.com>
2024-02-29 14:36:33 +0600

    connected backend

--- src/redux/api/baseAPI.ts
+++ src/redux/api/baseAPI.ts
@@ -2,6 +2,6 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
     reducerPath: 'baseApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://aidpulse-backend.vercel.app/' }),
     endpoints: () => ({}),
 });
